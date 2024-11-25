e430145815fd2f56906a152f7aa1db5b682be010
Mahtab6kn <mahtab6kn@gmail.com>
2024-11-25 12:35:53 +0530

    add the token with geolocation api

--- app/api/(public)/geolocation/route.js
+++ app/api/(public)/geolocation/route.js
@@ -1,7 +1,7 @@
 export async function GET(req) {
   try {
     // Using ipinfo.io as it's more reliable and has better free tier
-    const response = await fetch(`https://ipinfo.io/json/`);
+    const response = await fetch(`${process.env.GEOLOCATION_API_KEY}`);
     const data = await response.json();
     console.log({ Location: data });
     return Response.json(data);
