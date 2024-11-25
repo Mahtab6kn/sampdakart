a17670a5fd72376d5b0fc41ce84dba69b36cf82d
Mahtab6kn <mahtab6kn@gmail.com>
2024-11-25 12:44:02 +0530

    update the geolocation api

--- app/api/(public)/geolocation/route.js
+++ app/api/(public)/geolocation/route.js
@@ -1,7 +1,7 @@
 export async function GET(req) {
   try {
     // Using ipinfo.io as it's more reliable and has better free tier
-    const response = await fetch(`${process.env.GEOLOCATION_API_KEY}`);
+    const response = await fetch(`https://ipinfo.io/json?token=ab76a73d159b18`);
     const data = await response.json();
     console.log({ Location: data });
     return Response.json(data);
